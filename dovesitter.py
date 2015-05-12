341b97f8fc06b1e379dbe0cdf7659d68c51fbdfb
duderamos <eduardo@freedominterface.org>
2015-05-12 17:38:12 -0300

    Corrected identation. Implemented argument '-d' to run dovesitter as daemon. The default is foreground.

--- dovesitter.py
+++ dovesitter.py
@@ -25,6 +25,7 @@ import signal
 import threading
 import os, sys
 import signal
+import getopt
 
 import checkimap
 import director
@@ -32,80 +33,96 @@ import director
 UMASK=0
 WORKDIR = '/'
 RUNNING = True
-PIDFILE = '/tmp/dovesitter.pid'
-CONFFILE = 'dovesitter.conf'
+PIDFILE = '/var/run/dovesitter.pid'
+CONFDIR = '/etc/dovesitter/'
+CONFFILE = CONFDIR + './dovesitter.conf'
 
-logging.config.fileConfig('logging.conf')
+logging.config.fileConfig(CONFDIR + './logging.conf')
 logger = logging.getLogger('dovesitter')
 
 config = ConfigParser.ConfigParser()
 config.read(CONFFILE)
 
 def sighandler(signum, frame):
-        global RUNNING
-        if signum == signal.SIGTERM:
-                logger.info('Signal term received')
-                RUNNING = False
+    global RUNNING
+    if signum == signal.SIGTERM:
+        logger.info('Signal term received')
+        RUNNING = False
 
 def writepid(pid):
-        if os.path.isfile(PIDFILE):
-                return False
-        try:
-                fp = open(PIDFILE, 'w')
-                fp.write(str(pid))
-        except IOError:
-                return False
-        return True
+    if os.path.isfile(PIDFILE):
+        return False
+    try:
+        fp = open(PIDFILE, 'w')
+        fp.write(str(pid))
+    except IOError:
+        return False
+    return True
 
 def daemonize():
-        try:
-                pid = os.fork()
-        except OSError, e:
-                raise Exception, "%s [%d]" % (e.strerror, e.errno)
-
-        if (pid == 0):
-                if not writepid(os.getpid()):
-                        print "Check if there is another dovesitter instance running! " + PIDFILE
-                        os._exit(1)
-                os.setsid()
-                os.chdir(WORKDIR)
-                os.umask(UMASK)
+    try:
+        pid = os.fork()
+    except OSError, e:
+        raise Exception, "%s [%d]" % (e.strerror, e.errno)
+    
+    if (pid == 0):
+        if not writepid(os.getpid()):
+            print "Check if there is another dovesitter instance running! " + PIDFILE
+            os._exit(1)
+            os.setsid()
+            os.chdir(WORKDIR)
+            os.umask(UMASK)
         else:
-                os._exit(0)
-
+            os._exit(0)
+            
         try:
-                si = open('/dev/null', 'r')
-                so = open('/dev/null', 'a+')
-                se = open('/dev/null', 'a+', 0)
-                os.dup2(si.fileno(), sys.stdin.fileno())
-                os.dup2(so.fileno(), sys.stdout.fileno())
-                os.dup2(se.fileno(), sys.stderr.fileno())
+            si = open('/dev/null', 'r')
+            so = open('/dev/null', 'a+')
+            se = open('/dev/null', 'a+', 0)
+            os.dup2(si.fileno(), sys.stdin.fileno())
+            os.dup2(so.fileno(), sys.stdout.fileno())
+            os.dup2(se.fileno(), sys.stderr.fileno())
         except OSError:
-                pass
-                                
+            pass
+        
         return True
-
+    
 if __name__ == '__main__':
-        daemonize()
-        signal.signal(signal.SIGTERM, sighandler)
-	logger.info('Starting dovesitter main thread')
-	director = director.director(config.get('general', 'director_socket'))
-	threads = []
-	t = checkimap.checkimap(config.get('general', 'host_port'), director)
-	t.start()
-	threads.append(t)
-	while len(threads) > 0:
-		try:
-			for t in threads:
-                                if not RUNNING:
-                                        t.kill_received = True
-				if t is not None and t.isAlive():
-					t.join(1)
-				else:
-					threads.pop(threads.index(t))
-		except KeyboardInterrupt:
-			print "Ctrl-c received! Sending kill to threads..."
-			for t in threads:
-				t.kill_received = True
-	logger.info('Stopping...')
-	os.remove(PIDFILE)
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'd', [])
+    except getopt.GetoptError as err:
+        print str(err)
+        sys.exit(2)
+
+    DAEMON = False
+    for o, a in opts:
+        if o == '-d':
+            DAEMON = True
+            daemonize()
+        else:
+            assert False, 'unhandled option'
+
+    signal.signal(signal.SIGTERM, sighandler)
+    logger.info('Starting dovesitter main thread')
+    director = director.director(config.get('general', 'director_socket'))
+    threads = []
+    t = checkimap.checkimap(config.get('general', 'host_port'), director)
+    t.start()
+    threads.append(t)
+    while len(threads) > 0:
+        try:
+            for t in threads:
+                if not RUNNING:
+                    t.kill_received = True
+                if t is not None and t.isAlive():
+                    t.join(1)
+                else:
+                    threads.pop(threads.index(t))
+        except KeyboardInterrupt:
+            print "Ctrl-c received! Sending kill to threads..."
+            for t in threads:
+                t.kill_received = True
+    logger.info('Stopping...')
+    if DAEMON:
+        if(os.path.exists(PIDFILE)):
+            os.remove(PIDFILE)
